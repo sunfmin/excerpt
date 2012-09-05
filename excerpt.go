cd5dc863feb26d88a108cf196d815910c4ecbd57
Felix Sun <sunfmin@gmail.com>
2012-09-05 15:06:48 +0800

    finally merged highlighted sentences

--- excerpt.go
+++ excerpt.go
@@ -8,16 +8,42 @@ import (
 type highlightFunc func(word string) string
 
 func sentencesStop(w rune) (r bool) {
-	r = (strings.Index(",，.。?？!！\n", string(w)) >= 0)
+	r = (strings.Index(",，.。?？!！#\n", string(w)) >= 0)
 	return
 }
 
+type resultChunk struct {
+	before string
+	lines  []string
+	after  string
+}
+
+func (rc resultChunk) tostring() (r string) {
+	var rs []string
+	if rc.before != "" {
+		rs = append(rs, rc.before)
+	}
+	rs = append(rs, rc.lines...)
+	if rc.after != "" {
+		rs = append(rs, rc.after)
+	}
+	r = strings.Join(rs, ", ")
+	return
+}
+
+// Find the highlights sentences in each chunk of sources, and highlight the sentence and find the previous and next sentence around it, all connected highlighted sentences are merged into one
 func SentencesAround(sources []string, keywords []string, hf highlightFunc) (r []string) {
 	for _, chunk := range sources {
 		sentences := strings.FieldsFunc(chunk, sentencesStop)
-		previousHighlighted := false
+
+		rc := resultChunk{}
 
 		for _, s := range sentences {
+			if rc.after != "" {
+				r = append(r, rc.tostring())
+				rc = resultChunk{}
+			}
+
 			hs := strings.TrimSpace(s)
 			highlighted := false
 			for _, keyword := range keywords {
@@ -27,9 +53,19 @@ func SentencesAround(sources []string, keywords []string, hf highlightFunc) (r [
 					highlighted = true
 				}
 			}
+
 			if highlighted {
-				r = append(r, hs)
+				rc.lines = append(rc.lines, hs)
+			} else {
+				if len(rc.lines) > 0 {
+					rc.after = hs
+				} else {
+					rc.before = hs
+				}
 			}
+		}
+		if len(rc.lines) > 0 {
+			r = append(r, rc.tostring())
 		}
 	}
 	return
--- tests/excerpt_test.go
+++ tests/excerpt_test.go
@@ -18,16 +18,25 @@ Are you still going to use the cutout logo somewhere near the entrance of the of
 var cases = []Case{
 	{
 		[]string{p1},
-		[]string{"china", "sign"},
-		[]string{`We currently have only one *sign* at the outside of the building`, `I was just wondering where do you plan to put the *sign* in *China*`},
+		[]string{"entrance"},
+		[]string{
+			`I was just wondering where do you plan to put the sign in China, Are you going to put it at the *entrance* gate, or somewhere at the *entrance* of the building, Are you still going to use the cutout logo somewhere near the *entrance* of the office upstairs`,
+		},
 	},
 	{
 		[]string{p1},
-		[]string{"entrance"},
-		[]string{
-			`Are you going to put it at the *entrance* gate, or somewhere at the *entrance* of the building?`,
-			`still going to use the cutout logo somewhere near the entrance of the office upstairs?`,
-		},
+		[]string{"china", "sign"},
+		[]string{`We currently have only one *sign* at the outside of the building, I was just wondering where do you plan to put the *sign* in *China*, Are you going to put it at the entrance gate`},
+	},
+	{
+		[]string{p1},
+		[]string{"outside"},
+		[]string{`We currently have only one sign at the *outside* of the building, I was just wondering where do you plan to put the sign in China`},
+	},
+	{
+		[]string{p1},
+		[]string{"upstairs"},
+		[]string{`or somewhere at the entrance of the building, Are you still going to use the cutout logo somewhere near the entrance of the office *upstairs*`},
 	},
 }
 
@@ -41,11 +50,12 @@ func TestSentencesAround(t *testing.T) {
 	for _, c := range cases {
 		r := excerpt.SentencesAround(c.Source, c.Keywords, highlight)
 		if len(r) != len(c.Result) {
-			t.Error(r)
+			t.Errorf("expected: \n%s \n\nbut was: \n%s", c.Result, r)
+			continue
 		}
 		for i, line := range r {
 			if c.Result[i] != line {
-				t.Errorf("expected: \n%s, \n\nbut was: \n%s", c.Result[i], line)
+				t.Errorf("expected: \n%s \n\nbut was: \n%s", c.Result[i], line)
 			}
 		}
 	}
